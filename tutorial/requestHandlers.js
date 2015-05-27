3d4998bb5f73b55827d47ea4b14df065525f0212
Shawn Mealey <smealey@northpointdigital.com>
2015-05-27 11:13:22 -0400

    ADD - Finished off the Node Beginner Book!

--- tutorial/requestHandlers.js
+++ tutorial/requestHandlers.js
@@ -1,5 +1,7 @@
 var querystring = require("querystring"),
-    fs          = require("fs");
+    fs          = require("fs"),
+    formidable  = require('formidable');
+
 function start(response) {
   console.log("Request handler 'start' was called.");
   var body = '<html>'+
@@ -8,8 +10,8 @@ function start(response) {
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<form action="/upload"  enctype="multipart/form-data" method="post">'+
+    '<input type="file" name="upload" />'+
     '<input type="submit" value="Submit text" />'+
     '</form>'+
     '</body>'+
@@ -20,12 +22,25 @@ function start(response) {
 
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log(" Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: "+
-  querystring.parse(postData).text);
-  response.end();
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+
+    fs.rename(files.upload.path, "tmp/test.png", function(error) {
+      if (error) {
+        fs.unlink("tmp/test.png");
+        fs.rename(files.upload.path, "tmp/test.png");
+      }
+    });
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("recieved image: <br/>");
+    response.write("<img src='/show'/>");
+    response.end();
+  });
 }
 
 function show(response){
--- tutorial/router.js
+++ tutorial/router.js
@@ -1,7 +1,7 @@
-function route(handle, pathname, reponse, postData) {
+function route(handle, pathname, reponse, request) {
   console.log("About to route a request for "+pathname);
   if(typeof handle[pathname] === 'function') {
-    handle[pathname](reponse, postData);
+    handle[pathname](reponse, request);
   }
   else{
     console.log("No request handler found for " + pathname);
